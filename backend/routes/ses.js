0c1f4540b9e37f10b0c1c2f9de9f11e80f8a7d88
Dharmik2510 <47692702+Dharmik2510@users.noreply.github.com>
2022-07-27 12:16:22 -0300

    Update ses.js

--- backend/routes/ses.js
+++ backend/routes/ses.js
@@ -2,9 +2,7 @@ const AWS = require("aws-sdk");
 const router = require("express").Router();
 
 AWS.config.update({
-  // accessKeyId: "AKIA32UGKKHVBSQP33B4",
-  // secretAccessKey: "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",
-  // region: "us-east-1",
+ 
 });
 
 let SES = new AWS.SES();
